ea123706153e3d4a5f3bd39176d3fcb342a1f25e
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-12 15:40:58 -0300

    passagem de parametros

---
+++ src/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,15 @@
+package javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+    public void SomaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+
+    public void SubtraiDoisNumeros() {
+
+        System.out.println(15-10);
+    }
+    public void MultiplicaDoisNumeros(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
+}
