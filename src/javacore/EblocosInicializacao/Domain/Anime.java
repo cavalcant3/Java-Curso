668a8f1d773f505ff75b03b23bfcf81189c55ba3
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-11-16 21:08:04 -0300

    blocos de inicialização

---
+++ src/javacore/EblocosInicializacao/Domain/Anime.java
@@ -0,0 +1,12 @@
+package javacore.EblocosInicializacao.Domain;
+
+public class Anime {
+    private String nome;
+    private int[] episodios = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+    public Anime() {
+        System.out.println(episodios);
+    }
+
+
+}
---
+++ src/javacore/EblocosInicializacao/Test/AnimeTest01.java
@@ -0,0 +1,9 @@
+package javacore.EblocosInicializacao.Test;
+
+import javacore.EblocosInicializacao.Domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+    }
+}
