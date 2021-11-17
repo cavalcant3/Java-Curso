723936eb81ed81628a25875e34f38cd495cac4af
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-11-16 21:21:13 -0300

    bloco de inicialização

--- src/javacore/EblocosInicializacao/Domain/Anime.java
+++ src/javacore/EblocosInicializacao/Domain/Anime.java
@@ -2,10 +2,40 @@ package javacore.EblocosInicializacao.Domain;
 
 public class Anime {
     private String nome;
-    private int[] episodios = {1,2,3,4,5,6,7,8,9,10,11,12};
+    private int[] episodios;
+    {
+        // 1 - Alocado espaco em memória pro objeto
+        // 2 - Cada atributo de classe é criado e inicializado com valores default ou o quer for passado
+        // 3 - Bloco de inicialização é executado
+        // 4 - Construtor é executado
+        System.out.println("dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
 
     public Anime() {
         System.out.println(episodios);
+
+        for (int episodios: this.episodios) {
+            System.out.println(episodios + " ");
+
+        }
+
     }
 
 
--- src/javacore/EblocosInicializacao/Test/AnimeTest01.java
+++ src/javacore/EblocosInicializacao/Test/AnimeTest01.java
@@ -5,5 +5,6 @@ import javacore.EblocosInicializacao.Domain.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
+
     }
 }
